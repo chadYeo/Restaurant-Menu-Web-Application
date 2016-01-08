cfcc6c1f3848e391b32730b1c634ae7b5d5d2202
Chad Yeo <chad.yeo.cy@gmail.com>
2016-01-08 11:32:12 -0500

    Added more templates

--- database_setup.py
+++ database_setup.py
@@ -7,12 +7,21 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+class User(Base):
+	__tablename__ = 'user'
+
+	id = Column(Integer, primary_key = True)
+	name = Column(String(250),  nullable = False)
+	email = Column(String(250),  nullable = False)
+	picture = Column(String(250))
 
 class Restaurant(Base):
 	__tablename__ = 'restaurant'
 
 	id = Column(Integer, primary_key = True)
 	name = Column(String(250), nullable = False)
+	user_id = Column(Integer, ForeignKey('user.id'))
+	user = relationship(User)
 
 	# This is serialize function to send JSON objects in a serializable format
 	@property
@@ -32,6 +41,8 @@ class MenuItem(Base):
 	course = Column(String(250))
 	restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
 	restaurant = relationship(Restaurant)
+	user_id = Column(Integer, ForeignKey('user.id'))
+	user = relationship(User)
 
 
 # This is serialize function to send JSON objects in a serializable format
@@ -45,6 +56,6 @@ class MenuItem(Base):
 			'course': self.course
 		}
 
-engine = create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///restaurantmenuwithusers.db')
 
 Base.metadata.create_all(engine)
